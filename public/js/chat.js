f9d0dd55a67758f1d61ff9e46acfe146c3f6fda4
MOHIT SAINI <mohitsaininith@gmail.com>
2023-11-27 23:50:30 +0530

    basic socket.io setup done

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,1 +1,49 @@
-io()
+const socket = io()
+
+//DOM Elements
+
+const $messageForm = document.querySelector('#message-form');
+const $messageFormInput = $messageForm.querySelector('input');
+const $messageFormButton = $messageForm.querySelector('button');
+const $sendLocationButton = document.querySelector('#send-location');
+const $messages = document.querySelector('#messages');
+
+const $messageTemplate = document.querySelector('#message-template').innerHTML;
+socket.on('message',(msg)=>{
+    console.log(msg);
+}) 
+
+$messageForm.addEventListener('submit',(e)=>{
+    e.preventDefault();
+    $messageFormButton.setAttribute('disabled','disabled');
+
+    const msg = e.target.elements.message.value;
+    socket.emit('sendMessage',msg,(error)=>{
+        $messageFormButton.removeAttribute('disabled');
+        $messageFormInput.value='';
+        $messageFormInput.focus();
+
+        if(error){
+            return console.log(error);
+        }
+        console.log('Message delievered!');
+    });
+})
+
+$sendLocationButton.addEventListener('click',(e)=>{
+    if(!navigator.geolocation){
+        return alert('Geolocation is not supported by your browser');
+    }
+
+    $sendLocationButton.setAttribute('disabled','disabled');
+
+    navigator.geolocation.getCurrentPosition((position)=>{
+        socket.emit('sendLocation',{
+            latitude:position.coords.latitude,
+            longitude:position.coords.longitude
+        },(acknowledgement)=>{
+            console.log(acknowledgement);
+            $sendLocationButton.removeAttribute('disabled');
+        });
+    })
+})
--- src/index.js
+++ src/index.js
@@ -2,21 +2,51 @@ const path = require('path');
 const http = require('http');
 const express = require('express');
 const socketio = require('socket.io');
+const Filter = require('bad-words');
 
 const app = express();
 const server = http.createServer(app);
-const io = socketio(server);
+const io = socketio(server); 
 
 const port = process.env.PORT || 3000
 
 const publicDirectoryPath = path.join(__dirname,'../public');
 app.use(express.static(publicDirectoryPath));
-// app.get('/',(req,res)=>{
-//     res.send('say my name');
-// })
-io.on('connection',()=>{
-    console.log('New websocket connection')
+
+
+io.on('connection',(socket)=>{
+    console.log('New websocket connection');
+    socket.emit('message','Welcome!');
+
+    socket.broadcast.emit('message','A new user has joined!');
+
+    socket.on('sendMessage',(msg,callback)=>{
+        const filter = new Filter();
+        if(filter.isProfane(msg)){
+            return callback('profanity is not allowed!');
+        }
+        io.emit('message',msg);
+        callback('Delievered!');
+    })
+    socket.on('sendLocation',(coords,callback)=>{
+        setTimeout(()=>{
+            io.emit('message',`https://google.com/maps?q=${coords.latitude},${coords.longitude}`);
+            callback('Location shared!'); 
+        },2000)
+        // io.emit('message',`https://google.com/maps?q=${coords.latitude},${coords.longitude}`);
+        // callback('Location shared!');
+    })
+
+    socket.on('disconnect',()=>{
+        io.emit('message','A user has left!!');
+    })
 })
 server.listen(port,()=>{
     console.log('listening on port :',port);
 })
+
+
+
+//websocket allow for full duplex communication
+//websockt is a seperate protocol from HTTp
+//persistent connection bw client & server
